1f160e5caf6d46e50550939cee559385e1135b28
rangelo00 <reyes.ralphangelo@gmail.com>
2024-12-14 12:16:06 +0800

    mock default log-in to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get "dashboard/index"
+
+  authenticated :user do
+    root 'dashboard#index', as: :authenticated_root
+  end
+
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
