99bb0c999b3f0c9d21bce7546ac47b47707c0c84
Evandel Maylas <evandel@avionschool.com>
2025-01-06 20:06:41 +0800

    created search and show method

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -3,12 +3,19 @@ class PodcastsController < ApplicationController
     RSpotify.authenticate("2f8aa0ea29914151ab35bb287be9749c", "ec3bea7b485b4a11bfe097b71b82fa37")
 
     if params[:search].present?
-      @podcast = RSpotify::Show.search(params[:search])
+      @podcast = RSpotify::Episode.search(params[:search])
 
       respond_to do |format|
       format.html 
-      format.json { render json: @podcast } 
+      format.json { render json: @podcast }
       end
     end
+  end
+
+  def show
+    RSpotify.authenticate("2f8aa0ea29914151ab35bb287be9749c", "ec3bea7b485b4a11bfe097b71b82fa37")
+
+
+      @episode = RSpotify::Episode.find(params[:id])
   end
 end
