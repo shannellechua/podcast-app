1d42a287456f6bd9638cc65bfb88fd82c81ae7fd
Shannelle <shannellechua@gmail.com>
2025-01-14 21:08:50 +0800

    Tests

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -23,6 +23,7 @@ class PodcastsController < ApplicationController
     current_user.episodes.create(spotify_id: @episode.id, name: @episode.name, show: @episode.show.name)
     redirect_to podcasts_path, notice: 'Episode added to favorites!'
   end
+  
   def unfavorite
     @episode = RSpotify::Episode.find(params[:id])
     episode = current_user.episodes.find_by(spotify_id: @episode.id)
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,4 @@
 class Episode < ApplicationRecord
     # Change the belongs_to relationship to just validate the presence of podcast_id
-    belongs_to :user
-  end
+  belongs_to :user
+end
