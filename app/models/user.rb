10ad08c57f73b36ee90b19d8e62584507bc72bf7
Evandel Maylas <evandel@avionschool.com>
2025-01-16 17:42:42 +0800

    merge files

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-<<<<<<< HEAD
   has_many :subscriptions
-=======
   has_many :episodes
->>>>>>> main
 end
