f094c709fbaeb453b3608ee1fcad7be13b01099c
Evandel Maylas <evandel@avionschool.com>
2025-01-16 17:37:12 +0800

    added subscription model

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,31 @@
 class SubscriptionsController < ApplicationController
 
-    def index
-        @subscriptions = Subscription.all
+  def index
+    @subscriptions = Subscription.all
+  end
+
+  def create
+    show_id = params[:show_id]
+    show_name = params[:show_name]
+
+    @subscription = current_user.subscriptions.find_or_initialize_by(show_id: show_id)
+    @subscription.show_name = show_name
+
+    if @subscription.save
+      flash[:notice] = "Subscribed to #{show_name}!"
+    else
+      flash[:alert] = "Subscription failed."
     end
+    redirect_back fallback_location: root_path
+  end
+  
+  def destroy
+    subscription = current_user.subscriptions.find_by(show_id: params[:show_id])
+    if subscription&.destroy
+      flash[:notice] = "Unsubscribed successfully!"
+    else
+      flash[:alert] = "Failed to unsubscribe."
+    end
+    redirect_back fallback_location: root_path
+  end
 end
