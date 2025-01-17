39a45f71669b39a7529873ebbfa07d2d428765f3
Shannelle <shannellechua@gmail.com>
2025-01-17 14:11:00 +0800

    Added some styling to subscriptions page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,4 @@ class UsersController < ApplicationController
   def subscriptions
     @subscriptions = current_user.subscriptions.includes(:user)  # Get all subscriptions for the current user
   end
-
-  # def destroy
-  #   subscription = current_user.subscriptions.find(params[:id])
-  #   if subscription.destroy
-  #     flash[:notice] = "Unsubscribed successfully from #{subscription.show_name}."
-  #   else
-  #     flash[:alert] = "Failed to unsubscribe."
-  #   end
-  #   redirect_back(fallback_location: subscriptions_path)
-  # end
 end 
