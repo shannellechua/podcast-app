3e24aa7965d3c7132497d5fc14d66feb4a7000a8
Evandel Maylas <evandel@avionschool.com>
2025-02-26 23:59:44 +0800

    fixed user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_14_115136) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_26_154302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
